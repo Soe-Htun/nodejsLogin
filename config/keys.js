b387800ce97a902cacb381f776490833fbd06f91
Soe Htun <soehtuncu@gmail.com>
2020-09-04 16:49:55 +0630

    add login

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports ={
+    MongoURI: "mongodb+srv://soehtun123:soehtun37@cluster0.k6ssf.mongodb.net/<dbname>?retryWrites=true&w=majority"
+}
---
+++ routes/users.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+
+//User Model
+const User = require("../models/User");
+
+//Login Page
+router.get("/login", (req,res) => res.render("login"));
+
+//Register Page
+router.get("/register", (req,res) => res.render("register"));
+
+//login Handle
+
+//Register Handle
+router.post('/register', (req, res) => {
+    const { name, email, password, password2 } = req.body;
+    let errors =[];
+
+    //Check required fields
+    if(!name || !email || !password || !password2){
+        errors.push({
+            msg: "Please fill in all fields"
+        })
+    }
+
+    //Check password match
+    if(password !== password2){
+        errors.push({
+            msg:"Passwords do not match"
+        });
+    }
+
+    //Check password length
+    if(password.length < 6){
+        errors.push({
+            msg:"Psswords should be at least 6 characters"
+        });
+    }
+
+    if(errors.length > 0){
+        res.render("register", {
+            errors,
+            name,
+            email,
+            password,
+            password2
+        })
+    }
+    else{
+        //Validation Passed
+        User.findOne({ email: email })
+        .then(user => {
+            if(user){
+                errors.push({ msg: "Email is already registered"})
+                res.render("register", {
+                    errors,
+                    name,
+                    email,
+                    password,
+                    password2
+                })
+            }
+            else{
+                const newUser = new User({
+                    name,
+                    email,
+                    password
+                });
+                
+                //Hash Password
+                // bcrypt.genSalt(10, (err,salt) => 
+                //     bcrypt.hash(newUser.password, salt, (err, hash) => {
+                //         if(err) throw err;
+                //         //Set password to hashed
+                //         newUser.password = hash;
+                //         //Save user
+                //         newUser.save()
+                //         .then(user => {
+                //             req.flash('success_msg', 'You are now registered and can login')
+                //             res.redirect('/users/login');
+                //         })
+                //         .catch(err => console.log(err));
+                //     })
+                // )
+            }
+        })
+    }
+})
+
+
+module.exports = router;
